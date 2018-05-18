982129eb2e8321aeb787e7d8c1ddf644565a19d4
AARichburg <32150726+AARichburg@users.noreply.github.com>
2018-05-18 17:58:32 -0400

    Add files via upload

--- KL_div.py
+++ KL_div.py
@@ -6,18 +6,12 @@ Created on Tue May  8 14:39:20 2018
 @author: Richburg
 """
 
-import sys
-import argparse
 from collections import Counter
 from nltk.corpus import stopwords
-import time
 from collections import OrderedDict
 import math
 from nltk import ngrams
 import pickle
-#from user import User, Post
-from gensim import corpora, models
-#import gensim
 
 subreddits = ['Anger', 'BPD', 'EatingDisorders',
     'MMFB', 'StopSelfHarm', 'SuicideWatch', 'addiction', 'alcoholism', 'depression', 'feelgood', 'getting_over_it', 'hardshipmates',
@@ -93,8 +87,8 @@ def uni_prob_smth(text,vocab_size):
     count = Counter(text)
     L = float(vocab_size)
     prob_dict = {}
-    #k = 1/ float(L)
-    k = 1
+    k = 1/ float(L)
+    #k = 1
     for word in text:
         prob_word = (count[word] + k) / (L*k + float(L))
         prob_dict.update({word: prob_word})
@@ -117,13 +111,6 @@ def kl_div(distr1, distr2, vocab_size):
     kl_div_dict_sort = OrderedDict(sorted(kl_div_dict.items(), key = lambda item: item[1], reverse = True))
     return total_sum , kl_div_dict_sort
 
-def create_bows(docs_tokens):
-    dictionary = corpora.Dictionary(docs_tokens)
-    dictionary.filter_extremes(no_below=30, keep_n=5000, no_above=0.04)
-    bows = [dictionary.doc2bow(doc) for doc in docs_tokens]
-    #print('{} words'.format(len(dictionary.token2id)))
-    return bows, dictionary
-
 def remove_stopwords(text):
     new_list = []
     stop_words = set(stopwords.words('english'))
@@ -145,24 +132,9 @@ def help_make_title(P_or_Q):
         output = str('control')
     return output
 
-def get_collocations(docs):
-    # get collocations for documents
-    # returns documents but with bigrams (unigrams that are separated by _)
-    tokens = [doc.split() for doc in docs]
-    phrases = models.phrases.Phrases(tokens, min_count=100, threshold=10000.0, max_vocab_size=1000)
-    bigrams = [sentence.split() for sentence in phrases[docs]]
-    return bigrams
-
-
 if __name__ == '__main__':
-    #argparser = argparse.ArgumentParser()
-    #argparser.add_argument('--dist_1',type=str, required=True,
-	#						help="Data for distribution 1")
-    #argparser.add_argument('--dist_2', type=str, required=True,
-	#						help="Data for distribution 2")
-    
+    #Replace path with directory that contains test_users preprocessed dataset
     path = '/Users/Richburg/Dropbox/SPRING_18/LING_773/Assignments/FINAL/umd_reddit_suicidewatch_dataset/'
-    #args = argparser.parse_args()
     sub_path = path + '/reddit_annotation'
 
     pickle_obj = open(path+'test_users', 'rb')
@@ -171,6 +143,8 @@ if __name__ == '__main__':
     crowd_user_dict = remove_experts(user_dict)
     print(len(crowd_user_dict))
     
+    #P and Q represent dataset distributions; 
+    #1 is annnotated SW, 0 is unannotated SW, -1 is control
     P=1
     Q=0
     P_user_dict, Q_user_dict = split_data(crowd_user_dict,P,Q)
@@ -182,13 +156,13 @@ if __name__ == '__main__':
     new_docs_Q = [item for sublist in docs_Q for item in sublist]
     
     #Change n for the size of ngrams
-    n = 3
+    n = 2
     if n >= 2:
         flat_docs_P = []
         for docu in new_docs_P:
             words = docu.split()
             flat_docs_P.append(words)
-            for k in range(2,n+1):
+            for k in range(n,n+2):
                 grams = make_grams(words,k)
                 flat_docs_P.append(grams)
         
@@ -196,7 +170,7 @@ if __name__ == '__main__':
         for docu in new_docs_Q:
             words = docu.split()
             flat_docs_Q.append(words)
-            for k in range(2,n+1):
+            for k in range(n,n+2):
                 grams = make_grams(words,k)
                 flat_docs_Q.append(grams)
             
@@ -204,11 +178,8 @@ if __name__ == '__main__':
         flat_docs_Q = [word for doc in flat_docs_Q for word in doc]
     else:
         flat_docs_P = [word for doc in new_docs_P for word in doc.split()]
-        flat_docs_Q = [word for doc in new_docs_Q for wor in doc.split()]
-    
-    
-    #flat_docs_sw = remove_stopwords(new_docs_sw)
-    #flat_docs_con = remove_stopwords(new_docs_con)
+        flat_docs_Q = [word for doc in new_docs_Q for word in doc.split()]
+
     total_voc = flat_docs_P + flat_docs_Q
     vocab_size = len(Counter(total_voc))
     print(vocab_size)
@@ -220,25 +191,25 @@ if __name__ == '__main__':
     print(len(div_dict))
     print(len(div_dict2))
     
+    #max number of words contributing to KL divergence
     top_number = 600
     title_P = help_make_title(P)
     title_Q = help_make_title(Q)
     
-    div_file2 = 'top_' + str(top_number) +'_div_' + title_Q + '_to_' +  title_P + 'up_to_' + str(n)+ 'grams' +'.txt'
-    with open(div_file2, 'w') as f2:
-        f2.write('D(p||q) = ' + str(div2) + '\n')
-        runthru2 = list(div_dict2)
-        if top_number <= len(runthru2):
-            M = top_number
-        else:
-            M = len(runthru2)
-        for i in range(0,M):
-            word = runthru2[i]
-            div2 = float(div_dict2[word])
-            #row = [word, div]
-            f2.write(str(word) + ' ' + str(div2) + '\n')
+    #div_file2 = 'top_' + str(top_number) +'_div_' + title_Q + '_to_' +  title_P + '_' + str(n)+ 'grams' +'.txt'
+    #with open(div_file2, 'w') as f2:
+     #   f2.write('D(p||q) = ' + str(div2) + '\n')
+      #  runthru2 = list(div_dict2)
+       # if top_number <= len(runthru2):
+        #    M = top_number
+        #else:
+        #    M = len(runthru2)
+        #for i in range(0,M):
+         #   word = runthru2[i]
+          #  div2 = float(div_dict2[word])
+           # f2.write(str(word) + ' ' + str(div2) + '\n')
     
-    div_file = 'top_' + str(top_number) +'_div_' + title_P + '_to_' +  title_Q + 'up_to_' + str(n)+ 'grams' +'.txt'
+    div_file = 'top_' + str(top_number) +'_div_' + title_P + '_to_' +  title_Q + '_' + str(n)+ 'grams' +'.txt'
     with open(div_file, 'w') as f:
         f.write('D(p||q) = ' + str(div) + '\n')
         runthru1 = list(div_dict)
@@ -249,6 +220,5 @@ if __name__ == '__main__':
         for i in range(0,M):
             word = runthru1[i]
             div = float(div_dict[word])
-            #row = [word, div]
             f.write(str(word) + ' ' + str(div) + '\n')
     
